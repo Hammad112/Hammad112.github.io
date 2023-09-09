d38d36b5849cec5d3746a4477f2f93998d5bbb38
Hammad Nasir <95902997+Hammad112@users.noreply.github.com>
2023-09-09 00:28:38 -0700

    Update SpeakGoodBye.js

--- module_4_Solution/SpeakGoodBye.js
+++ module_4_Solution/SpeakGoodBye.js
@@ -26,6 +26,7 @@
   window.byeSpeaker = byeSpeaker;
 })(window);
 
+
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
